ae0f3bddcb94e530c4ecbd667bba45a482b4ef75
panghu <792559738@qq.com>
2019-10-08 08:25:39 +0800

    平衡二叉树的左旋和右旋

--- src/tree/AVLTree.java
+++ src/tree/AVLTree.java
@@ -1,21 +1,22 @@
 package tree;
 
+import set.FileOperation;
+
 import java.util.ArrayList;
 
 public class AVLTree<K extends Comparable<K>, V> {
 
-    private class Node {
+    private class Node{
         public K key;
         public V value;
         public Node left, right;
         public int height;
 
-        public Node(K key, V value) {
+        public Node(K key, V value){
             this.key = key;
             this.value = value;
             left = null;
             right = null;
-            //进行添加的位置一定是叶子节点的位置,所以新的Node节点height为1
             height = 1;
         }
     }
@@ -23,42 +24,40 @@ public class AVLTree<K extends Comparable<K>, V> {
     private Node root;
     private int size;
 
-    public AVLTree() {
+    public AVLTree(){
         root = null;
         size = 0;
     }
 
-    public int getSize() {
+    public int getSize(){
         return size;
     }
 
-    public boolean isEmpty() {
+    public boolean isEmpty(){
         return size == 0;
     }
 
-    //判断该树是否是一棵平衡二叉树
-    public boolean isBST() {
+    // 判断该二叉树是否是一棵二分搜索树
+    public boolean isBST(){
 
         ArrayList<K> keys = new ArrayList<>();
-        inOrder(root,keys);
-        for (int i = 0; i < keys.size(); i++) {
-            if(keys.get(i - 1).compareTo(keys.get(i)) > 0) {
+        inOrder(root, keys);
+        for(int i = 1 ; i < keys.size() ; i ++)
+            if(keys.get(i - 1).compareTo(keys.get(i)) > 0)
                 return false;
-            }
-        }
         return true;
-
     }
 
-    private void inOrder(Node node, ArrayList<K> keys) {
+    private void inOrder(Node node, ArrayList<K> keys){
 
-        if (node == null) {
+        if(node == null)
             return;
-        }
-        inOrder(node.left,keys);
+
+        inOrder(node.left, keys);
         keys.add(node.key);
         inOrder(node.right, keys);
     }
+
     // 判断该二叉树是否是一棵平衡二叉树
     public boolean isBalanced(){
         return isBalanced(root);
@@ -67,17 +66,29 @@ public class AVLTree<K extends Comparable<K>, V> {
     // 判断以Node为根的二叉树是否是一棵平衡二叉树，递归算法
     private boolean isBalanced(Node node){
 
-        if(node == null) {
+        if(node == null)
             return true;
-        }
 
         int balanceFactor = getBalanceFactor(node);
-        if(Math.abs(balanceFactor) > 1) {
+        if(Math.abs(balanceFactor) > 1)
             return false;
-        }
         return isBalanced(node.left) && isBalanced(node.right);
     }
 
+    // 获得节点node的高度
+    private int getHeight(Node node){
+        if(node == null)
+            return 0;
+        return node.height;
+    }
+
+    // 获得节点node的平衡因子
+    private int getBalanceFactor(Node node){
+        if(node == null) {
+            return 0;
+        }
+        return getHeight(node.left) - getHeight(node.right);
+    }
 
     // 对节点y进行向右旋转操作，返回旋转后新的根节点x
     //        y                              x
@@ -125,57 +136,48 @@ public class AVLTree<K extends Comparable<K>, V> {
         return x;
     }
 
-
-
-    // 获得节点node的高度
-    private int getHeight(Node node) {
-        if (node == null) {
-            return 0;
-        }
-        return node.height;
-    }
-
-    // 获得节点node的平衡因子
-    private int getBalanceFactor(Node node) {
-        if (node == null)
-            return 0;
-        return getHeight(node.left) - getHeight(node.right);
-    }
-
     // 向二分搜索树中添加新的元素(key, value)
-    public void add(K key, V value) {
+    public void add(K key, V value){
         root = add(root, key, value);
     }
 
     // 向以node为根的二分搜索树中插入元素(key, value)，递归算法
     // 返回插入新节点后二分搜索树的根
-    private Node add(Node node, K key, V value) {
+    private Node add(Node node, K key, V value){
 
-        if (node == null) {
-            size++;
+        if(node == null){
+            size ++;
             return new Node(key, value);
         }
 
-        if (key.compareTo(node.key) < 0) {
+        if(key.compareTo(node.key) < 0)
             node.left = add(node.left, key, value);
-        } else if (key.compareTo(node.key) > 0) {
+        else if(key.compareTo(node.key) > 0)
             node.right = add(node.right, key, value);
-        } else // key.compareTo(node.key) == 0
-        {
+        else // key.compareTo(node.key) == 0
             node.value = value;
-        }
 
         // 更新height
         node.height = 1 + Math.max(getHeight(node.left), getHeight(node.right));
 
         // 计算平衡因子
         int balanceFactor = getBalanceFactor(node);
-        // 平衡维护
-        if (balanceFactor > 1 && getBalanceFactor(node.left) >= 0) {
+
+        // 平衡维护  分为四种不平衡的情况
+
+        if (balanceFactor > 1 && getBalanceFactor(node.left) >= 0)
+            return rightRotate(node);
+
+        if (balanceFactor < -1 && getBalanceFactor(node.right) <= 0)
+            return leftRotate(node);
+
+        if (balanceFactor > 1 && getBalanceFactor(node.left) < 0) {
+            node.left = leftRotate(node.left);
             return rightRotate(node);
         }
 
-        if (balanceFactor < -1 && getBalanceFactor(node.right) <= 0) {
+        if (balanceFactor < -1 && getBalanceFactor(node.right) > 0) {
+            node.right = rightRotate(node.right);
             return leftRotate(node);
         }
 
@@ -183,52 +185,52 @@ public class AVLTree<K extends Comparable<K>, V> {
     }
 
     // 返回以node为根节点的二分搜索树中，key所在的节点
-    private Node getNode(Node node, K key) {
+    private Node getNode(Node node, K key){
 
-        if (node == null)
+        if(node == null)
             return null;
 
-        if (key.equals(node.key))
+        if(key.equals(node.key))
             return node;
-        else if (key.compareTo(node.key) < 0)
+        else if(key.compareTo(node.key) < 0)
             return getNode(node.left, key);
         else // if(key.compareTo(node.key) > 0)
             return getNode(node.right, key);
     }
 
-    public boolean contains(K key) {
+    public boolean contains(K key){
         return getNode(root, key) != null;
     }
 
-    public V get(K key) {
+    public V get(K key){
 
         Node node = getNode(root, key);
         return node == null ? null : node.value;
     }
 
-    public void set(K key, V newValue) {
+    public void set(K key, V newValue){
         Node node = getNode(root, key);
-        if (node == null)
+        if(node == null)
             throw new IllegalArgumentException(key + " doesn't exist!");
 
         node.value = newValue;
     }
 
     // 返回以node为根的二分搜索树的最小值所在的节点
-    private Node minimum(Node node) {
-        if (node.left == null)
+    private Node minimum(Node node){
+        if(node.left == null)
             return node;
         return minimum(node.left);
     }
 
     // 删除掉以node为根的二分搜索树中的最小节点
     // 返回删除节点后新的二分搜索树的根
-    private Node removeMin(Node node) {
+    private Node removeMin(Node node){
 
-        if (node.left == null) {
+        if(node.left == null){
             Node rightNode = node.right;
             node.right = null;
-            size--;
+            size --;
             return rightNode;
         }
 
@@ -237,42 +239,44 @@ public class AVLTree<K extends Comparable<K>, V> {
     }
 
     // 从二分搜索树中删除键为key的节点
-    public V remove(K key) {
+    public V remove(K key){
 
         Node node = getNode(root, key);
-        if (node != null) {
+        if(node != null){
             root = remove(root, key);
             return node.value;
         }
         return null;
     }
 
-    private Node remove(Node node, K key) {
+    private Node remove(Node node, K key){
 
-        if (node == null)
+        if( node == null )
             return null;
 
-        if (key.compareTo(node.key) < 0) {
-            node.left = remove(node.left, key);
+        if( key.compareTo(node.key) < 0 ){
+            node.left = remove(node.left , key);
             return node;
-        } else if (key.compareTo(node.key) > 0) {
+        }
+        else if(key.compareTo(node.key) > 0 ){
             node.right = remove(node.right, key);
             return node;
-        } else {   // key.compareTo(node.key) == 0
+        }
+        else{   // key.compareTo(node.key) == 0
 
             // 待删除节点左子树为空的情况
-            if (node.left == null) {
+            if(node.left == null){
                 Node rightNode = node.right;
                 node.right = null;
-                size--;
+                size --;
                 return rightNode;
             }
 
             // 待删除节点右子树为空的情况
-            if (node.right == null) {
+            if(node.right == null){
                 Node leftNode = node.left;
                 node.left = null;
-                size--;
+                size --;
                 return leftNode;
             }
 
@@ -288,30 +292,6 @@ public class AVLTree<K extends Comparable<K>, V> {
 
             return successor;
         }
-    }
-
-    public static void main(String[] args) {
-
-        System.out.println("Pride and Prejudice");
-
-        ArrayList<String> words = new ArrayList<>();
-        if (FileOperation.readFile("/home/panghu/IdeaProjects/algorithm-data-structure/src/pride-and-prejudice.txt", words)) {
-            System.out.println("Total words: " + words.size());
-
-            AVLTree<String, Integer> map = new AVLTree<>();
-            for (String word : words) {
-                if (map.contains(word))
-                    map.set(word, map.get(word) + 1);
-                else
-                    map.add(word, 1);
-            }
-
-            System.out.println("Total different words: " + map.getSize());
-            System.out.println("Frequency of PRIDE: " + map.get("pride"));
-            System.out.println("Frequency of PREJUDICE: " + map.get("prejudice"));
-        }
-
-        System.out.println();
     }
 
 }
