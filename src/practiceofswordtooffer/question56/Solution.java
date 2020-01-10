13257fdf7b928794a2fa07a3ee03ded67343fbe2
panghu <792559738@qq.com>
2020-01-10 21:14:40 +0800

    question56 数组中数字出现的次数

--- src/practiceofswordtooffer/question56/Solution.java
+++ src/practiceofswordtooffer/question56/Solution.java
@@ -4,4 +4,36 @@ package practiceofswordtooffer.question56;
  * @author dengg
  */
 public class Solution {
+
+    public void findNumsAppearOnce(int[] arr,int num1,int num2) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+
+        //异或的结果
+        int resultExclusiveOR = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            resultExclusiveOR ^= arr[i];
+        }
+
+        int indexOf1 =findFirstBitIs1(resultExclusiveOR);
+
+        num1 = num2 = 0;
+    }
+
+    private int findFirstBitIs1(int num) {
+        int indexBit = 0;
+        while (((num & 1) == 0) && (indexBit < 8 * Integer.SIZE)) {
+            num = num >> 1;
+            ++ indexBit;
+        }
+        return indexBit;
+    }
+
+    private boolean isBit1(int num,int indexBit) {
+        num = num >> indexBit;
+        return (num & 1) > 0;
+    }
+
 }
