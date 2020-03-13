41ac65630164fa3cf2cd15fc1ea5e52738987588
panghu <792559738@qq.com>
2020-03-13 18:06:39 +0800

    丑数 -- 空间换时间

--- src/practiceofswordtooffer/question49/Solution.java
+++ src/practiceofswordtooffer/question49/Solution.java
@@ -1,5 +1,7 @@
 package practiceofswordtooffer.question49;
 
+import org.junit.Test;
+
 /**
  * @author dengg
  */
@@ -12,12 +14,14 @@ public class Solution {
 
         //所谓的潜在位置指的就是该位置（对应的丑数分别乘以2、3、5后）大于已知最大丑数最小值的位置
         //初始化三个指向三个潜在成为最小丑数的位置
+        // 会将前面的每一个数字都乘一遍
+        // 每一个因子相乘都保留在原位置  result[p]存储了最小的因子
         int p2 = 0,p3 = 0,p5 = 0;
         int[] result = new int[index];
         //第一个丑数是1
         result[0] = 1;
         for (int i = 1; i < index; i++) {
-            //丑数一定是三个潜在最小丑数中的最小值
+            //丑数一定是三个潜在最小丑数中的最小值，
             result[i] = Math.min(result[p2]*2, Math.min(result[p3]*3, result[p5]*5));
 
             //为了防止重复需要三个if都能够走到
@@ -34,6 +38,11 @@ public class Solution {
         }
 
         return result[index-1];
+    }
+    @Test
+    public void test() {
+       int result = GetUglyNumber_Solution(17);
+        System.out.println(result);
     }
 
 }
---
+++ src/practiceofswordtooffer/question49/Solution_Copy.java
@@ -0,0 +1,42 @@
+package practiceofswordtooffer.question49;
+
+/**
+ * @author: panghu
+ * @Description:
+ * @Date: Created in 17:29 2020/3/13
+ * @Modified By:
+ */
+public class Solution_Copy {
+
+    public int getUglyNumberSolution(int index) {
+        //参数验证
+        if (index < 0) {
+            return -1;
+        }
+
+        //初始化三个指向三个潜在成为最小丑数的位置 -- 重点理解
+        //
+        int p2 = 0,p3 = 0,p5 = 0;
+        int[] result = new int[index];
+        //第一个丑数是1
+        result[0] = 1;
+        for (int i = 0; i < index; i++) {
+            //丑数一定是三个潜在最小丑数中的最小值
+            result[i] = Math.min(result[p2]*2, Math.min(result[p3]*3, result[p5]*5));
+
+            //判断是那个最小因子
+            if (result[i] == result[p2]) {
+                p2++;
+            }
+            if(result[i] == result[p3]*3) {
+                p3++;
+            }
+            if(result[i] == result[p5]*5) {
+                p5++;
+            }
+        }
+
+        return result[index-1   ];
+    }
+
+}
