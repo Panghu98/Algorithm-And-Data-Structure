76a45b985a41a3dde7207a611247e79261c46110
panghu <792559738@qq.com>
2021-01-10 21:06:07 +0800

    子集问题

--- src/leetcode/leetcode78.java
+++ src/leetcode/leetcode78.java
@@ -1,11 +1,38 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import org.junit.Test;
+
+import java.util.*;
 
 public class leetcode78 {
 
+    List<List<Integer>> res = new ArrayList<>();
 
+    public List<List<Integer>> subsets(int[] nums) {
+        LinkedList<Integer> list = new LinkedList<>();
+        getSubSet(nums,0,list);
+
+        return res;
+    }
+
+    private void getSubSet(int[] nums,int start,LinkedList<Integer> list) {
+
+        res.add(new LinkedList<>(list));
+
+
+        for (int i = start; i < nums.length; i++) {
+            list.add(nums[i]);
+            getSubSet(nums,i + 1, list);
+            list.removeLast();
+        }
+
+    }
+
+    @Test
+    public void test() {
+        List<List<Integer>> subsets = subsets(new int[]{1, 2, 3});
+        System.err.println(subsets.size());
+        subsets.forEach(System.out::println);
+    }
 
 }
